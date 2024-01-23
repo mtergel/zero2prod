303758b4c365066013c62b4d4a09f869c41440d7
Tergel Munkhdelger <tergelm@gmail.com>
2024-01-23 17:45:47 +0800

    feat: telemetry

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::*;
 
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
