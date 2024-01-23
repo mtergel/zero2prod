b852f57dc59cde9a1771c0180fcc06324c50813b
Tergel Munkhdelger <61941297+mtergel@users.noreply.github.com>
2024-01-23 17:54:14 +0800

    feat/telemetry (#2)
    * feat: telemetry
    * bugfix: clippy string fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use sqlx::PgPool;
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let config = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&config.database.connection_string())
+    let connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
