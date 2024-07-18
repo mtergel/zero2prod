0da65a5aa8844ec912dc90d7c836094b9f62bd68
Tergel M <tergel.m@cody.mn>
2024-07-18 08:57:25 +0800

    feat: added timeout for email_client

--- docs/general.md
+++ docs/general.md
@@ -8,6 +8,11 @@ cargo watch -x check -x test -x run
 
 ### Tests
 
+A good test suite is, first and foremost, a risk-mitigation measure.
+Automated tests reduce the risk associated with changes to an existing codebase -
+most regressions and bugs are caught in the CI pipeline and never reach users.
+The team is therefore empowered to iterate faster and release more often.
+
 - Embedded
   This module has privileged access to the code living next to it, structs,
   methods, fields, functions that have not been marked as public. Use these to
@@ -18,6 +23,7 @@ cargo watch -x check -x test -x run
 We converted the binary into a library to share the code for testing.
 
 Inner Development Loop
+
 - Make a change;
 - Compile the application;
 - Run tests;
--- src/email_client.rs
+++ src/email_client.rs
@@ -14,11 +14,9 @@ impl EmailClient {
         base_url: String,
         sender: SubscriberEmail,
         authorization_token: Secret<String>,
+        timeout: std::time::Duration,
     ) -> Self {
-        let http_client = Client::builder()
-            .timeout(std::time::Duration::from_secs(10))
-            .build()
-            .unwrap();
+        let http_client = Client::builder().timeout(timeout).build().unwrap();
 
         Self {
             http_client,
@@ -115,7 +113,12 @@ mod tests {
     }
 
     fn email_client(base_url: String) -> EmailClient {
-        EmailClient::new(base_url, email(), Secret::new(Faker.fake()))
+        EmailClient::new(
+            base_url,
+            email(),
+            Secret::new(Faker.fake()),
+            std::time::Duration::from_millis(200),
+        )
     }
 
     #[tokio::test]
@@ -159,8 +162,7 @@ mod tests {
     #[tokio::test]
     async fn send_email_fails_if_the_server_returns_500() {
         let mock_server = MockServer::start().await;
-        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
-        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+        let email_client = email_client(mock_server.uri());
 
         Mock::given(any())
             .respond_with(ResponseTemplate::new(500))
