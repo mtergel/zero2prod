58429c53f18dfed87b2a28c8839281648a84de15
Tergel Munkhdelger <tergelm@gmail.com>
2024-08-14 21:14:59 +0800

    feat: auth module

---
+++ src/routes/login/mod.rs
@@ -0,0 +1,10 @@
+use actix_web::{http::header::ContentType, HttpResponse};
+
+pub async fn login_form() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("login.html"))
+}
+
+mod post;
+pub use post::login;
